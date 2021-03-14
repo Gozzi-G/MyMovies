be1b4aba08afa0bc3643befba8dc821e7b05decf
Gozzy-G <struck.yury@gmail.com>
2021-03-14 12:11:00 +0400

    add fields to class JSONUtils

--- app/src/main/java/com/example/mymovies/utils/JSONUtils.java
+++ app/src/main/java/com/example/mymovies/utils/JSONUtils.java
@@ -1,5 +1,25 @@
 package com.example.mymovies.utils;
 
+import com.example.mymovies.data.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JSONUtils {
-    
+    private static final String KEY_RESULTS = "results";
+    private static final String KEY_ID = "id";
+    private static final String KEY_VOTE_COUNT = "vote_count";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_ORIGINAL_TITLE = "original_title";
+    private static final String KEY_OVERVIEW = "overview";
+    private static final String KEY_POSTER_PATH = "poster_path";
+    private static final String KEY_BACKDROP_PATH = "backdrop_path";
+    private static final String KEY_VOTE_AVERAGE = "vote_average";
+    private static final String KEY_RELEASE_DATE = "release_date";
+
+
+
 }

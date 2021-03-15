45ed4aa06cd06e3ae79e25905fdc4739e24ac47f
Gozzy-G <struck.yury@gmail.com>
2021-03-15 09:21:17 +0400

    Add method setMethodOfSort
    The method allow switch between Popularity and the most rated movies, also when switching the text color is changes

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.mymovies.utils.JSONUtils;
 import com.example.mymovies.utils.NetworkUtils;
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewPosters;
     private MovieAdapter movieAdapter;
+    private Switch switchSort;
+    private TextView textViewTopRated;
+    private TextView textViewPopularity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +31,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        switchSort = findViewById(R.id.switchSort);
+        textViewPopularity = findViewById(R.id.textViewPopularity);
+        textViewTopRated = findViewById(R.id.textViewTopRated);
+
         recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
         movieAdapter = new MovieAdapter();
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 1);
-        ArrayList<Movie> movies = JSONUtils.getMovieFromJSON(jsonObject);
-        movieAdapter.setMovies(movies);
         recyclerViewPosters.setAdapter(movieAdapter);
 
+        switchSort.setChecked(true);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               setMethodOfSort(isChecked);
+            }
+        });
+        switchSort.setChecked(false);
+    }
 
+    public void onClickSetPopularity(View view) {
+        setMethodOfSort(false);
+        switchSort.setChecked(false);
+    }
+
+    public void onClickSetTopRated(View view) {
+        setMethodOfSort(true);
+        switchSort.setChecked(true);
+    }
+
+    private void setMethodOfSort(boolean isTopRated) {
+        int methodOfSort;
+        if(isTopRated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            textViewTopRated.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.white_color));
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+            textViewPopularity.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewTopRated.setTextColor(getResources().getColor(R.color.white_color));
+        }
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 2);
+        ArrayList<Movie> movies = JSONUtils.getMovieFromJSON(jsonObject);
+        movieAdapter.setMovies(movies);
     }
 }
